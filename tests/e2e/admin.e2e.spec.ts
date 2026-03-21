e9fc0caf8ac97b26c4d0dbebc89e95a00b15dee8
michxymi <michxymi@gmail.com>
2026-03-21 19:32:29 +0000

    fix: update test environment and fix linting errors on tests

--- tests/e2e/admin.e2e.spec.ts
+++ tests/e2e/admin.e2e.spec.ts
@@ -1,41 +1,46 @@
-import { test, expect, Page } from '@playwright/test'
-import { login } from '../helpers/login'
-import { seedTestUser, cleanupTestUser, testUser } from '../helpers/seedUser'
+import { expect, type Page, test } from "@playwright/test";
+import { login } from "../helpers/login";
+import { cleanupTestUser, seedTestUser, testUser } from "../helpers/seed-user";
 
-test.describe('Admin Panel', () => {
-  let page: Page
+const USERS_EDIT_VIEW_URL_PATTERN =
+  /\/admin\/collections\/users\/[a-zA-Z0-9-_]+/;
 
-  test.beforeAll(async ({ browser }, testInfo) => {
-    await seedTestUser()
+test.describe("Admin Panel", () => {
+  let page: Page;
 
-    const context = await browser.newContext()
-    page = await context.newPage()
+  test.beforeAll(async ({ browser }) => {
+    await seedTestUser();
 
-    await login({ page, user: testUser })
-  })
+    const context = await browser.newContext();
+    page = await context.newPage();
+
+    await login({ page, user: testUser });
+  });
 
   test.afterAll(async () => {
-    await cleanupTestUser()
-  })
+    await cleanupTestUser();
+  });
 
-  test('can navigate to dashboard', async () => {
-    await page.goto('http://localhost:3000/admin')
-    await expect(page).toHaveURL('http://localhost:3000/admin')
-    const dashboardArtifact = page.locator('span[title="Dashboard"]').first()
-    await expect(dashboardArtifact).toBeVisible()
-  })
+  test("can navigate to dashboard", async () => {
+    await page.goto("http://localhost:3000/admin");
+    await expect(page).toHaveURL("http://localhost:3000/admin");
+    const dashboardArtifact = page.locator('span[title="Dashboard"]').first();
+    await expect(dashboardArtifact).toBeVisible();
+  });
 
-  test('can navigate to list view', async () => {
-    await page.goto('http://localhost:3000/admin/collections/users')
-    await expect(page).toHaveURL('http://localhost:3000/admin/collections/users')
-    const listViewArtifact = page.locator('h1', { hasText: 'Users' }).first()
-    await expect(listViewArtifact).toBeVisible()
-  })
+  test("can navigate to list view", async () => {
+    await page.goto("http://localhost:3000/admin/collections/users");
+    await expect(page).toHaveURL(
+      "http://localhost:3000/admin/collections/users"
+    );
+    const listViewArtifact = page.locator("h1", { hasText: "Users" }).first();
+    await expect(listViewArtifact).toBeVisible();
+  });
 
-  test('can navigate to edit view', async () => {
-    await page.goto('http://localhost:3000/admin/collections/users/create')
-    await expect(page).toHaveURL(/\/admin\/collections\/users\/[a-zA-Z0-9-_]+/)
-    const editViewArtifact = page.locator('input[name="email"]')
-    await expect(editViewArtifact).toBeVisible()
-  })
-})
+  test("can navigate to edit view", async () => {
+    await page.goto("http://localhost:3000/admin/collections/users/create");
+    await expect(page).toHaveURL(USERS_EDIT_VIEW_URL_PATTERN);
+    const editViewArtifact = page.locator('input[name="email"]');
+    await expect(editViewArtifact).toBeVisible();
+  });
+});
--- tests/e2e/frontend.e2e.spec.ts
+++ tests/e2e/frontend.e2e.spec.ts
@@ -1,20 +1,15 @@
-import { test, expect, Page } from '@playwright/test'
+import { expect, test } from "@playwright/test";
 
-test.describe('Frontend', () => {
-  let page: Page
+const TITLE_REGEX = /Payload Blank Template/;
 
-  test.beforeAll(async ({ browser }, testInfo) => {
-    const context = await browser.newContext()
-    page = await context.newPage()
-  })
+test.describe("Frontend", () => {
+  test("can go on homepage", async ({ page }) => {
+    await page.goto("http://localhost:3000");
 
-  test('can go on homepage', async ({ page }) => {
-    await page.goto('http://localhost:3000')
+    await expect(page).toHaveTitle(TITLE_REGEX);
 
-    await expect(page).toHaveTitle(/Payload Blank Template/)
+    const headging = page.locator("h1").first();
 
-    const headging = page.locator('h1').first()
-
-    await expect(headging).toHaveText('Welcome to your new project.')
-  })
-})
+    await expect(headging).toHaveText("Welcome to your new project.");
+  });
+});
